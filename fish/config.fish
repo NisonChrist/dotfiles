d677d73d91694c0fb34cfa72832c5f0a748b3661
Ziv <nison1225@qq.com>
2026-02-22 10:56:01 +0800

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,12 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    # fastfetch
-    # tmux a
+  # Commands to run in interactive sessions can go here
+  zoxide init fish | source
+  starship init fish | source
 end
+
+fish_vi_key_bindings
+
+set fish_greeting ""
 
 abbr -a vi vim
 abbr -a nv nvim
@@ -10,9 +14,4 @@ abbr -a cl clear
 abbr -a ll lsd -l
 abbr -a t tmux
 
-set fish_greeting ""
-
-test -r '/Users/ziv/.opam/opam-init/init.fish' && source '/Users/ziv/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
-
 # set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
-starship init fish | source
