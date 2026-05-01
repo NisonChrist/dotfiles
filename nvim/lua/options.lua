9c80df4bc3cd52b335242a6d9f11aa7a7027f062
Ziv <nison1225@qq.com>
2026-05-01 11:12:12 +0800

    Update options.lua

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,6 +1,6 @@
 vim.opt.number = true         -- 显示行号
 vim.opt.relativenumber = true -- 显示相对行号
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = "yes:2"
 
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
@@ -11,5 +11,5 @@ vim.opt.clipboard = "unnamedplus"
 
 vim.opt.swapfile = false
 
-vim.opt.cursorline = false
+vim.opt.cursorline = true
 vim.opt.termguicolors = true
