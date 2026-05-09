8ba647d5a925a1212f9c7bf1e922800c0657ce14
Ziv <nison1225@qq.com>
2026-05-09 15:00:48 +0800

    Remove unnecessary signcolumn width option

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,6 +1,7 @@
 vim.opt.number = true         -- 显示行号
 vim.opt.relativenumber = true -- 显示相对行号
-vim.opt.signcolumn = "yes:2"
+vim.opt.signcolumn = "yes"
+-- vim.opt.signcolumn = "yes:2"
 
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
