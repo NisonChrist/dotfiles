f13f8f2432076b2f3d07eb44d560497feebb5f81
Ziv <nison1225@qq.com>
2026-05-24 15:57:56 +0800

    Add NvimTree cursorline highlight

--- nvim/plugin/nvim-tree.lua
+++ nvim/plugin/nvim-tree.lua
@@ -1,7 +1,9 @@
 vim.pack.add({
-  { src = "https://github.com/nvim-tree/nvim-tree.lua", name = "nvim-tree" }
+	{ src = "https://github.com/nvim-tree/nvim-tree.lua", name = "nvim-tree" },
 })
 
 require("nvim-tree").setup()
-
+vim.cmd([[
+      :hi      NvimTreeCursorLine guibg=None guifg=#f5c2e7
+]])
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>")
