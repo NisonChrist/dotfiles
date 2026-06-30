2ec3373115d08b8991becf2b2171a42145753b28
Ziv <nison1225@qq.com>
2026-06-30 12:54:31 +0800

    Refactor interactive fish config and add abbreviations

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,18 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
+  set fish_greeting ""
+
+  fish_vi_key_bindings
+
+  abbr -a vi "vim"
+  abbr -a nv "nvim"
+  abbr -a cl "clear"
+  abbr -a ff "fastfetch"
+  abbr -a t "tmux"
+
+  set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
   starship init fish | source
+
   zoxide init fish | source
 end
 
-fish_vi_key_bindings
-
-set fish_greeting ""
-
-abbr -a vi "vim"
-abbr -a nv "nvim"
-abbr -a cl "clear"
-abbr -a ff "fastfetch"
-abbr -a t "tmux"
-
-set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
